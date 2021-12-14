86e249e4c5dcdf761fd3baabd7011e30b192d557
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-12-14 15:00:32 +0800

    Create author-sol.cpp

---
+++ author-sol.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		vector<int> b(n);
+		for (int i = 0; i < n; i++) {
+			cin >> b[i];
+		}
+		// view the image in this repository for better understanding
+		// for each 1 <= i < N, B[i] must be divisible by B[i + 1]
+		bool checker = true;
+		for (int i = 0; i + 1 < n; i++) {
+			checker &= (b[i] % b[i + 1] == 0);
+		}
+		if (checker) {
+			for (int i = 0; i < n; i++) {
+				cout << b[i] << " ";
+			}
+		} else {
+			cout << -1;
+		}
+		cout << '\n';
+	}
+	return 0;
+}
